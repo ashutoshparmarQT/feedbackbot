466e0e3e08281ebbcc877f09c501f556f182eb08
Ashutosh <ashutosh.parmar@gmail.com>
2016-11-23 17:21:21 +0530

    New Project Structure

---
+++ feedbackbot/src/feedbackbot.service/Models/FeedbackContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace feedbackbot.service.Models
+{
+    public class FeedbackContext : DbContext
+    {
+        public FeedbackContext() : base()
+        {
+            //TODO: How to pass connection strings to base class
+        }
+
+        public FeedbackContext(string connectionString) : base()
+        {
+            //TODO: How to pass connection strings to base class
+        }
+
+
+    }
+}
