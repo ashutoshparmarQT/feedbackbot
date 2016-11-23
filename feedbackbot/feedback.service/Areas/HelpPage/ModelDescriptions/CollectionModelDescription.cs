8abd729a69ade03dcc66cac90960eaed2c6ed100
Ashutosh <ashutosh.parmar@gmail.com>
2016-11-23 12:29:05 +0530

    Adding initial project structure

---
+++ feedbackbot/feedback.service/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace feedback.service.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
